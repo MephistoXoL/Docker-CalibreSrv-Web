e6516e2089dd4d825bca8fde9e0a4e18816d33d4
MephistoXoL <47185433+MephistoXoL@users.noreply.github.com>
2019-10-25 22:22:46 +0200

    notificarion system changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 BOOKS_SCRIPT="/app/Auto_Books_Calibre.sh"
-TELEGRAM_MSG="tgram://$TOKEN/$CHATID"
 NOTIFICATIONS=$(echo $NOTIFICATIONS | tr a-z A-Z)
 
 # Checking Notifications are enabled
@@ -10,15 +9,17 @@ echo "# Checking Notifications  #"
 echo "###########################"
 echo ""
 if [ "$NOTIFICATIONS" = "ENABLED" ]; then
-        echo "Notifications enabled"
+        echo "# Notifications enabled"
         echo "# Adding Telegram info to Script..."
-        sed -i 's,'"Telegram_input"','"apprise $TELEGRAM_MSG -b"',' $BOOKS_SCRIPT
+        sed -i 's,'"TOKENBOT"','"$TOKEN"',' $BOOKS_SCRIPT
+        sed -i 's,'"CHATIDBOT"','"$CHATID"',' $BOOKS_SCRIPT
         echo "# Token = $TOKEN"
         echo "# CHATID = $CHATID"
 fi
 echo ""
-echo "# Adding cronjob..."
+echo "# Adding cronjob and starting..."
 echo '0,15,30,45 * * * * /app/Auto_Books_Calibre.sh >> /Books_Calibre_Backup/01-Calibre.log 2>&1' | crontab
+service cron start
 echo ""
 echo "###########################"
 echo "# Config completed        #"
